978c38f189a6148969cea1ea7328d0c9f4eb19f1
kanapuliAthavan <athavankanapuli@gmail.com>
2017-12-11 06:39:02 +0530

    add logic to compare logger size and bytestobeWritten

--- logger.go
+++ logger.go
@@ -2,7 +2,9 @@ package logger
 
 import (
 	"errors"
+	"fmt"
 	"io"
+	"os"
 	"sync"
 	"time"
 )
@@ -17,15 +19,26 @@ type Logger struct {
 	//LocalTime value decides if the file to be written using system's local time
 	LocalTime bool `json:"localtime"`
 	mu        sync.Mutex
+	file      *os.File
+	size      int64
 }
 
 var (
 	currentTime = time.Now()
 )
 
-//Write implements the io.Writer
+//Write implements the io.Writer. Write checks if the dataToWrite is greater than the max filesize of logger.
 func (l *Logger) Write(data []byte) (int, error) {
-	return 1, errors.New("Not Implemented")
+	lengthToWrite := int64(len(data))
+	if lengthToWrite > l.size {
+		return -1, fmt.Errorf("The file size %d is greater than the max logger size %d", lengthToWrite, l.size)
+	}
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	bytesWritten, err := l.file.Write(data)
+	l.size += int64(bytesWritten)
+	return bytesWritten, err
 }
 
 //Close implements Closer
