8406792a25c281f7b3cef0558338fcb5d397aa77
Athavan kanapuli <athavankanapuli@gmail.com>
2018-03-14 23:08:10 +0530

    add createLogFile method for Logger type

--- logger.go
+++ logger.go
@@ -3,15 +3,20 @@ package logger
 import (
 	"context"
 	"io"
+	"log"
+	"os"
+	"sync"
 	"time"
 )
 
 //Logger represents the active logging properties
 type Logger struct {
-	Filename  string    `json:"filename"`
-	Timestamp time.Time `json:"timestamp"`
-	FileSize  int       `json:"filesize"` //Filesize is in Megabytes
-	Filepath  string    `json:"filepath"`
+	Filename  string    `json:"filename"`  //FileName refers to Log File name
+	Timestamp time.Time `json:"timestamp"` //Timestamp is the epoch timestamp when the log is received
+	FileSize  int       `json:"filesize"`  //FileSize is the size of the file in Megabytes
+	Filepath  string    `json:"filepath"`  //FilePath is the path of the log file
+	file      *os.File
+	mu        sync.Mutex
 }
 
 // LogLevel is the integer referring to INFO,DEBUG etc
@@ -40,10 +45,26 @@ func (logger *Logger) Close() error {
 
 //Write writes to the log file
 func (logger *Logger) Write(data []byte) (int, error) {
+	if logger.file == nil {
+		logger.createLogFile()
+	}
 	return 0, nil
 }
 
 //Log is the external interface to get log data
 func Log(ctx context.Context, level LogLevel, data interface{}) error {
 	return nil
+}
+
+func (logger *Logger) createLogFile() {
+	path := logger.Filepath + logger.Filename
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		file, err := os.Create(path)
+		if err != nil {
+			log.Fatalf("Error in creating log file: %v", err)
+		}
+		logger.file = file
+	}
+
 }
